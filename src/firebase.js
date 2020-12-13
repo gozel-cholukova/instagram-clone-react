5aedf6e3a9f47ddc6ff1f5c6b5f97307f0e22d07
Gozel Cholukova <62962282+gozel-cholukova@users.noreply.github.com>
2020-12-13 21:46:12 +0500

    Add files via upload

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyA13IdWNSB_z7IuGzBTK1cFrcTOuMejjMc",
+  authDomain: "instagram-clone-react-8f859.firebaseapp.com",
+  databaseURL: "https://instagram-clone-react-8f859-default-rtdb.firebaseio.com",
+  projectId: "instagram-clone-react-8f859",
+  storageBucket: "instagram-clone-react-8f859.appspot.com",
+  messagingSenderId: "844871740921",
+  appId: "1:844871740921:web:e3aab7349181b00d2c2128",
+  measurementId: "G-9KX37Q4EPL"
+};
