2f3373f2c84c251e2359fea9edfecf6709ee3d48
Gozel Cholukova <62962282+gozel-cholukova@users.noreply.github.com>
2020-12-17 02:01:29 +0500

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -2,9 +2,38 @@ import React, { useState, useEffect } from 'react';
 import './App.css';
 import Post from './Post';
 import { db } from './firebase';
+import { makeStyles } from '@material-ui/core/styles';
+import Modal from '@material-ui/core/Modal';
+import { Button } from '@material-ui/core';
+
+function getModalStyle() {
+  const top = 50;
+  const left = 50;
+
+  return {
+    top: `${top}%`,
+    left: `${left}%`,
+    transform: `translate(-${top}%, -${left}%)`,
+  };
+}
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    position: 'absolute',
+    width: 400,
+    backgroundColor: theme.palette.background.paper,
+    border: '2px solid #000',
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+  },
+}));
 
 function App() {
+  const classes = useStyles();
+  const [modalStyle] = useState(getModalStyle);
+
   const [posts, setPosts] = useState ([]);
+  const [open, setOpen] = useState(false);
 
   // useEffect -> Runs a piece of code based on a specific condition
 
@@ -12,12 +41,37 @@ function App() {
     //this is where the code runs
     db.collection('posts').onSnapshot(snapshot => {
       //every time a new post is added, this code runs..
-      setPosts(snapshot.docs.map(doc => doc.data()));
+      setPosts(snapshot.docs.map(doc => ({
+        id: doc.id,
+        post: doc.data()
+      })));
     })
   }, []);
 
+
+  const signUp = (event) => {
+
+
+  }
+
+
   return (
     <div className="App">
+      <Modal
+        open={open}
+        onClose={() => setOpen(false)}
+      >
+        <div style={modalStyle} className={classes.paper}>
+          <center>
+          <img 
+            className="app__headerImg" 
+            src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png" 
+            alt="">
+          </img>
+          </center>
+        </div>
+      </Modal>
+
       <div className="app__header">
         <img 
         className="app__headerImg" 
@@ -27,11 +81,13 @@ function App() {
 
       </div>
 
+      <Button onClick={() => setOpen(true)}>Sign Up</Button>
+
       <h1>HELLO Insta World :rock!</h1>
 
       {
-        posts.map(post => (
-          <Post username={post.username} caption={post.caption} imageUrl={post.imageUrl}></Post>
+        posts.map(({id, post}) => (
+          <Post key={id} username={post.username} caption={post.caption} imageUrl={post.imageUrl}></Post>
         ))
       }
 
