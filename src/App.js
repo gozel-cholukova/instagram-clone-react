89768ad96a82a65425019c354b8894d6475390f4
Gozel Cholukova <62962282+gozel-cholukova@users.noreply.github.com>
2020-12-18 02:44:43 +0500

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
 import Post from './Post';
-import { db } from './firebase';
+import { db, auth } from './firebase';
 import { makeStyles } from '@material-ui/core/styles';
 import Modal from '@material-ui/core/Modal';
-import { Button } from '@material-ui/core';
+import { Button, Input } from '@material-ui/core';
 
 function getModalStyle() {
   const top = 50;
@@ -34,8 +34,40 @@ function App() {
 
   const [posts, setPosts] = useState ([]);
   const [open, setOpen] = useState(false);
+  const [openSignIn, setOpenSignIn] = useState(false);
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState('');
+  const [user, setUser] = useState(null);
 
   // useEffect -> Runs a piece of code based on a specific condition
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((authUser) => {
+      if (authUser) {
+        //user has logged in
+        console.log(authUser);
+        setUser(authUser);
+        // if (authUser.displayName) {
+        //   //dont update username
+        // } else {
+        //   //if we just created someone
+        //   return authUser.updateProfile({
+        //     displayName: username,
+        //   });
+        // }
+      } else {
+        //user has logged out
+        setUser(null);
+      }
+    }) 
+    
+    return () => {
+      //perform some cleanup actions
+      unsubscribe();
+    }
+  }, [user, username]);
+
 
   useEffect(() => {
     //this is where the code runs
@@ -50,25 +82,54 @@ function App() {
 
 
   const signUp = (event) => {
+    event.preventDefault();
 
-
+    auth.createUserWithEmailAndPassword(email, password)
+    .then((authUser) => {
+      return authUser.user.updateProfile({
+        displayName: username
+      })    
+    })
+    .catch((error) => alert(error.message));
   }
 
-
   return (
-    <div className="App">
+    <div className="app">
       <Modal
         open={open}
         onClose={() => setOpen(false)}
       >
         <div style={modalStyle} className={classes.paper}>
-          <center>
-          <img 
-            className="app__headerImg" 
-            src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png" 
-            alt="">
-          </img>
-          </center>
+          <form className="app__signup">
+            <center>
+              <img 
+                className="app__headerImg" 
+                src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png" 
+                alt=""
+              />
+            </center>
+
+            <Input
+            type="text"
+            placeholder="username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            />
+            <Input
+            placeholder="email"
+            type="text"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            />
+            <Input
+            placeholder="password"
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            />        
+            <Button type="submit" onClick={signUp}>Sign Up</Button>
+          </form> 
+
         </div>
       </Modal>
 
@@ -78,10 +139,17 @@ function App() {
         src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png" 
         alt="">
         </img>
-
       </div>
 
-      <Button onClick={() => setOpen(true)}>Sign Up</Button>
+      {user ? (
+        <Button onClick={() => auth.signOut()}>Logout</Button>
+      ): (
+        <div className="app__loginContainer">
+          <Button onClick={() => setOpenSignIn(true)}>Sign In</Button>
+          <Button onClick={() => setOpen(true)}>Sign Up</Button>
+        </div>
+        
+      )}      
 
       <h1>HELLO Insta World :rock!</h1>
 
@@ -91,7 +159,7 @@ function App() {
         ))
       }
 
-    </div>
+    </div>    
   );
 }
 
