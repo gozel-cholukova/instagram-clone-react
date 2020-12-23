2ef7e7210c5f446149fb253e6f22cdd66afebd83
Gozel Cholukova <62962282+gozel-cholukova@users.noreply.github.com>
2020-12-23 21:57:41 +0500

    Add files via upload

--- src/Post.js
+++ src/Post.js
@@ -1,8 +1,33 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './Post.css';
 import Avatar from "@material-ui/core/Avatar";
+import { db } from './firebase';
 
-function Post({ username, caption, imageUrl }) {
+function Post({ postId, username, caption, imageUrl }) {
+  const [comments, setComments] = useState([]);
+  const [comment, setComment] = useState('');
+
+  useEffect(() => {
+    let unsubscribe;
+    if (postId) {
+      unsubscribe = db
+      .collection("posts")
+      .doc(postId)
+      .collection("comments")
+      .onSnapshot((snapshot) => {
+        setComments(snapshot.docs.map((doc) => doc.data()));
+      });
+    }
+
+    return () => {
+      unsubscribe();
+    };
+  }, [postId]);
+
+  const postComment = (event) => {
+
+  }
+
   return (
     <div className="post">
       <div className="post__header">
@@ -11,14 +36,42 @@ function Post({ username, caption, imageUrl }) {
           alt='GozelCholukova' 
           src="/static/images/avatar/3.jpg" 
         />
-
-      <h3>{username}</h3>
+        <h3>{username}</h3>
       </div>
       
 
     <img className="post__img" src={imageUrl} alt=""/>
 
-    <h4 className="post__text"><strong>{username} </strong>{caption}</h4>
+    <h4 className="post__text"><strong>{username} </strong> {caption}</h4>
+
+    
+    <div className="post__comments">
+      {comments.map((comment) => (
+        <p>
+          <strong>{comment.username}</strong> {comment.text}
+        </p>
+      ))}
+    </div>
+    
+
+    <form className="post__commentBox">
+      <input
+        className="post__input"
+        type="text"
+        placeholder="Add a comment..."
+        value={comment}
+        onChange={(e) => setComment(e.target.value)}>
+      </input>
+
+      <button
+        className="post__button"
+        disabled={!comment}        
+        type="submit"
+        onClick={postComment}
+      >
+        Post
+      </button>
+    </form>
 
     </div>
   )
