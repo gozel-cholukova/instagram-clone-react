b1fe74fc5ef585fe30a2d0183446ed65f6676c7f
Gozel Cholukova <62962282+gozel-cholukova@users.noreply.github.com>
2020-12-25 21:55:34 +0500

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,13 @@
 import firebase from "firebase";
 
 const firebaseApp = firebase.initializeApp({
-  apiKey: "AIzaSyA13IdWNSB_z7IuGzBTK1cFrcTOuMejjMc",
+  apiKey: ".......",
   authDomain: "instagram-clone-react-8f859.firebaseapp.com",
   databaseURL: "https://instagram-clone-react-8f859-default-rtdb.firebaseio.com",
   projectId: "instagram-clone-react-8f859",
   storageBucket: "instagram-clone-react-8f859.appspot.com",
   messagingSenderId: "844871740921",
-  appId: "1:844871740921:web:e3aab7349181b00d2c2128",
+  appId: "-------",
   measurementId: "G-9KX37Q4EPL"
 });
 
